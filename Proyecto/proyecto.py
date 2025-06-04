5bcf1a921664080863b9c1c7e8649b9620b6d916
Rch-0 <rodrigochaura5@gmail.com>
2025-06-04 00:56:40 -0400

    proyecto.py

--- Proyecto/proyecto.py
+++ Proyecto/proyecto.py
@@ -2,21 +2,21 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-ventana_x=1280
-ventana_y=720
+ventana_x=1024 #tamaño de la ventana
+ventana_y=712
 
 ventana=pygame.display.set_mode((ventana_x,ventana_y))
-pygame.display.set_caption("Prototipo")
+pygame.display.set_caption("Prototipo") #nombre
 reloj=pygame.time.Clock()
 
 def fondo_pantalla():
-    ventana.blit(Fondoimagen, (460,180))
+    ventana.blit(Fondoimagen, (0,0))#resolucion de imagen
     pygame.display.update()
 
 repetir=True
-while repetir:
+while repetir:#
 
-    Fondoimagen=pygame.image.load('Proyecto/imagenes/mogus.jpg')
+    Fondoimagen=pygame.image.load('Proyecto/imagenes/mogus.jpg')#ubicación de la imagen de fondo
 
     esta_jugando=True
     while esta_jugando:
